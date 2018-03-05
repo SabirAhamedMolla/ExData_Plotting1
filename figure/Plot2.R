e76700fef45c3f2be26642690dec4275270bd32b
Sabir_A <34520660+SabirAhamedMolla@users.noreply.github.com>
2018-03-05 13:56:29 +0530

    Create Plot2.R

---
+++ figure/Plot2.R
@@ -0,0 +1,6 @@
+## Create Plot 2
+
+plot(mydata$Global_active_power~mydata$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+#dev.copy(png,"plot2.png", width=480, height=480)
+#dev.off()
